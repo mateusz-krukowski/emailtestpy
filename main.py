87945771040657597abb4b1224390baecb1469ff
Mateusz <92203063+thorlaksson@users.noreply.github.com>
2023-02-23 21:05:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import ssl
+from smtplib import SMTP
+from email.message import EmailMessage
+from getpass import getpass
+
+fromaddr = ("sender's adress: ")
+toaddrs = input("receiver's adress: ")
+msg = EmailMessage()
+msg['From'] = fromaddr
+msg['To'] = toaddrs
+msg['Subject'] = "Sent via Python smtplib"
+msg.set_content("Siema Krzychu")
+password = getpass(prompt="type password: ")
+
+try:
+    context = ssl.create_default_context()
+    with SMTP("smtp.mail.me.com", 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls(context=context)
+        smtp.ehlo()
+        smtp.login(fromaddr,password)
+        smtp.sendmail(fromaddr,toaddrs,msg.as_string())
+except Exception as e:
+    print(e)
